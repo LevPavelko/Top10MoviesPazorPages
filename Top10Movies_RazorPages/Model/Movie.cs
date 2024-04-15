c64bfc3575f4c41aa752202daf1462a393b39133
LevPavelko <lev.pavelko.studies@gmail.com>
2024-04-15 17:30:20 +0200

    Task1: Создание модели и контекста

---
+++ Top10Movies_RazorPages/Model/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Top10Movies_RazorPages.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        public string Description { get; set; }
+
+        public string? Poster { get; set; }
+    }
+}
